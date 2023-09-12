101fe1123932228768067842ed896521fa8daee2
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-12 23:43:28 +0430

    second commit

---
+++ main2.js
@@ -0,0 +1,20 @@
+
+let a = 2
+let b = 5
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+console.log(a ** b);
+console.log(b % a);
+
+
+console.log(a--);
+console.log(--a);
+
+
+console.log(b--);
+console.log(--b);
+
