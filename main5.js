5291d243959d8f6eecede5399df93c3a9320d353
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 00:21:13 +0430

    fix main5

--- main5.js
+++ main5.js
@@ -1,10 +1,11 @@
 let age2 = 25;
+let name2 = "arash";
 
 
 var resuleAge = ""
-if (age2 == 20) {
+if (age2 == 20 && name2.startsWith("a")) {
     resuleAge = "age is 20"
-} else if (age2 > 20) {
+} else if (age2 > 20 && name2.startsWith("a")) {
     resuleAge = "age is above 20"
 } else {
     resuleAge = "age is below 20"
