2c9667c04dbd4d2dc8c92c8d946996e00396e8a4
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-13 23:44:03 +0430

    aaa 1

---
+++ main4.js
@@ -0,0 +1,28 @@
+//&&   ||     !=
+
+let isLogin = false;
+let isRegister = true;
+
+if (isLogin && isRegister) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if (isLogin || isRegister) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if (isLogin != isRegister) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if (isLogin == isRegister) {
+    console.log(true);
+} else {
+    console.log(false);
+}
