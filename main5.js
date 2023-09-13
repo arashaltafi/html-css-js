ff8125364b12b9b8507c81f3861b25babde888b8
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 00:11:42 +0430

    add main5

---
+++ main5.js
@@ -0,0 +1,13 @@
+let age2 = 25;
+
+
+var resuleAge = ""
+if (age2 == 20) {
+    resuleAge = "age is 20"
+} else if (age2 > 20) {
+    resuleAge = "age is above 20"
+} else {
+    resuleAge = "age is below 20"
+}
+
+console.log(resuleAge)
