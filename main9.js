795002b7fd18c07752d1cf56b1e8bc0af296924c
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 01:16:23 +0430

    main8 fix

--- main9.js
+++ main9.js
@@ -20,3 +20,13 @@ function min(a, b) {
 
 console.log("max ", max(2, 5))
 console.log("min ", min(2, 5))
+
+
+
+function checkNum(number) {
+    if (typeof number != "number") return "input type is not number!!!"
+
+    return (number % 2 == 0) ? "number is even" : "number is odd"
+}
+
+console.log("checkNum ", checkNum(11))
