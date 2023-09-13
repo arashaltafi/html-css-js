61a06684aa9f6be25831dcdb7fcb72191d0276d7
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 01:11:11 +0430

    main8 fix

---
+++ main9.js
@@ -0,0 +1,22 @@
+//max , min
+
+function max(a, b) {
+    // if (a > b) {
+    //     return a;
+    // } else{
+    //     return b;
+    // }
+    return (a > b) ? a : b
+}
+
+function min(a, b) {
+    // if (a < b) {
+    //     return a;
+    // } else{
+    //     return b;
+    // }
+    return (a < b) ? a : b;
+}
+
+console.log("max ", max(2, 5))
+console.log("min ", min(2, 5))
