90aa934fba85f96997f6621ec388e1ffc7602ce5
arash <arashaltafi1377@gmail.com>
2023-09-13 13:01:36 +0330

    commit 4

---
+++ main2.js
@@ -0,0 +1,29 @@
+console.log("main 2 js");
+
+
+let a = 2;
+let b = 7;
+
+console.log("a + b");
+console.log(a + b);
+console.log("a - b");
+console.log(a - b);
+console.log("a * b");
+console.log(a * b);
+console.log("a / b");
+console.log(a / b);
+
+
+// a ^ b
+console.log("a ** b");
+console.log(a ** b);
+
+
+let c = 10;
+console.log("10++");
+console.log(c++);
+
+
+let d = 10;
+console.log("++10");
+console.log(++d);
