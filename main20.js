4dff516895f1736f68fdc01ffd4e7fc60a23d115
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-15 01:05:05 +0430

    add main14

---
+++ main20.js
@@ -0,0 +1,14 @@
+const names = ["aaa", "bbb", "ccc", "ddd", "eee"];
+
+//for
+for (index of names) {
+    console.log(index)
+}
+
+console.log("/////////////////////////")
+
+//foreach
+names.forEach(element => {
+    console.log(element)
+});
+
