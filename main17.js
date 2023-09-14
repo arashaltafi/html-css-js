5459b3d7e92d5bc0540e7baa88f01762e7371a7d
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 23:55:25 +0430

    add main14

--- main17.js
+++ main17.js
@@ -12,3 +12,13 @@ points.unshift(8, 9)
 points.splice(2, 0, "arash")
 
 console.log(points)
+
+/////////////////////////////
+
+//find
+
+const points2 = [1, 2, 4, 5, 6, 4, 2, 35, 73, 6];
+console.log(points2.indexOf(2))
+console.log(points2.indexOf(2, 4))
+console.log(points2.includes(3))
+console.log(points2.includes(4))
