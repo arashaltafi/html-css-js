38c6eaaad87f51a55874a11579033e3870f5cfed
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 12:28:11 +0430

    add main10

---
+++ main10.js
@@ -0,0 +1,16 @@
+//property
+//method
+
+const circle = {
+    radiod: 10,
+    location: {
+        x: 2,
+        y: 3
+    },
+    isVisible: true,
+    move: function draw() {
+        console.log("move function")
+    }
+}
+
+circle.move()
