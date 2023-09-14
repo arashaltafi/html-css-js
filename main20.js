40e16e3827e7f13262b9a67923a65b2c55980f29
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-15 01:20:59 +0430

    add main14

--- main20.js
+++ main20.js
@@ -26,3 +26,20 @@ const filter = numbers.filter(number => {
     return number > 15;
 })
 console.log("filter", filter)
+
+
+
+//filter with foreach for objects
+const users = [
+    { name: "Arash", isActive: true, age: 25 },
+    { name: "hhh", isActive: false, age: 42 },
+    { name: "fff", isActive: false, age: 16 },
+    { name: "ttt", isActive: true, age: 63 },
+    { name: "nnn", isActive: false, age: 50 },
+]
+const activedUser = users.filter(name => {
+    return name.isActive
+})
+activedUser.forEach(element => {
+    console.log(element.name)
+});
