aa3ea195bd759fd8a90131c600ebac29c3c53417
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 18:10:45 +0430

    add main14

---
+++ main14.js
@@ -0,0 +1,20 @@
+const message = `I'm Arash Altafi`;
+
+let includes = message.includes("Arash");
+console.log(includes)
+
+let replace = message.replace("Arash", "Altafi");
+console.log(replace)
+
+let split = message.split(" ");
+console.log(split)
+
+let toUpperCase = message.toUpperCase();
+console.log(toUpperCase)
+
+let toLowerCase = message.toLowerCase();
+console.log(toLowerCase)
+
+
+let indexOf = message.indexOf("Arash");
+console.log(indexOf)
---
+++ main15.js
@@ -0,0 +1,12 @@
+const numbers = 20;
+
+console.log(Math.random());
+
+console.log(Math.min(1,2,5,9,5,6));
+
+console.log(Math.max(1,2,5,9,5,6));
+
+
+console.log(Math.round(2.4));
+console.log(Math.floor(2.8));
+console.log(Math.ceil(2.1));
