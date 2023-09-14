72f0f86a08f7b06948e45733e250fca8030b8b4c
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 12:50:15 +0430

    fix main10

--- main10.js
+++ main10.js
@@ -1,8 +1,7 @@
 //property
 //method
-
 const circle = {
-    radiod: 10,
+    radius: 10,
     location: {
         x: 2,
         y: 3
@@ -15,7 +14,34 @@ const circle = {
 
 circle.move()
 
-
+//////////////////////////////////////////
 
 //factory function
+function createCircle(radius, locationX, locationY, isVisible) {
+    return {
+        radius,
+        location: {
+            x: locationX,
+            y: locationY
+        },
+        isVisible: isVisible,
+        draw() {
+            console.log("Circle radius is ", radius)
+        }
+    }
+}
+
+createCircle(10, 2, 3, true).draw()
+createCircle(25, 2, 3, true).draw()
+
+//////////////////////////////////////////
+
 //constructor function
+function Circle2(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log("Circle radius is ", radius)
+    }
+}
+const circle1 = new Circle2(3).draw()
+const circle2 = new Circle2(5).draw()
