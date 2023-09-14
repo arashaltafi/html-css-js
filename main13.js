ae574fd221ff3c77aa57e21b18407e5af288428c
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-14 17:40:06 +0430

    add main13

--- main13.js
+++ main13.js
@@ -7,11 +7,24 @@ const users = {
 
 const users2 = {};
 
+
+// way 1
 for (key in users) {
     users2[key] = users[key]
 }
 
 users2.age = 26
 
+console.log("users2", users2)
+console.log("users", users)
+
+console.log("--------------------")
+
+
+//way 2
+const users3 = Object.assign({}, users);
+users3.age = 30
+
+console.log("users3", users3)
 console.log("users2", users2)
 console.log("users", users)
