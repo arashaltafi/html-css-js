33e15668a38c8c8f89eb14c2246cd52f9223eba3
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-15 00:47:55 +0430

    add main14

---
+++ main18.js
@@ -0,0 +1,16 @@
+const message = "I'm Arash Altafi";
+
+//replace
+const replace = message.replace("Arash", "aaa");
+console.log("replace", replace);
+
+
+//split
+const split = message.split(" ");
+console.log("split", split);
+
+
+//join
+const join = split.join("-");
+console.log("join", join);
+
