809e88eb74e639470e9e0de303d4dbe5bab5e3b5
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-15 01:16:06 +0430

    add main14

--- main20.js
+++ main20.js
@@ -5,10 +5,24 @@ for (index of names) {
     console.log(index)
 }
 
+
 console.log("/////////////////////////")
+
+
 
 //foreach
 names.forEach(element => {
     console.log(element)
 });
 
+
+console.log("/////////////////////////")
+
+
+
+//filter
+const numbers = [12, 12, 3, 132, 4, 324, 45, 234, , 4312, 3, 124];
+const filter = numbers.filter(number => {
+    return number > 15;
+})
+console.log("filter", filter)
