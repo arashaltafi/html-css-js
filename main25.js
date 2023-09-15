833b6ef216e9baa7345382ef82519f8ff755bebd
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-15 23:08:23 +0430

    a

---
+++ main25.js
@@ -0,0 +1,12 @@
+const h1 = document.getElementsByClassName("h1-1");
+console.log(h1)
+
+const h2 = document.getElementById("h1-2");
+console.log(h2)
+
+const root = document.querySelector(".root");
+console.log(root)
+
+const p1 = document.querySelector(".root .p-1");
+console.log(p1)
+
