5b6e072e6a971f963edbca8a5a8d8764a579ee14
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-16 17:26:13 +0430

    a

--- main27.js
+++ main27.js
@@ -1,5 +1,15 @@
 const btnAdd = document.querySelector("#button-add");
 const input = document.querySelector("#search-product");
+const body = document.querySelector("body");
+let root = document.createElement("root")
+
+const products = [
+    { title: "React.js is a JavaScript library developed by Facebook for building user interfaces, particularly single-page applications where UI components update dynamically." },
+    { title: "Node.js is a JavaScript runtime environment that allows developers to run JavaScript on the server-side, enabling the development of powerful, scalable, and networked applications." },
+    { title: "Next.js is a framework built on top of React.js that enables server-side rendering and provides a seamless developer experience for building modern web applications." },
+    { title: "TypeScript is a superset of JavaScript that adds static typing to the language, making it more robust and maintainable for larger-scale projects." },
+    { title: "JavaScript is a versatile and widely-used programming language for creating dynamic and interactive web applications." }
+]
 
 //event click => on click listener
 btnAdd.addEventListener("click", (event) => {
@@ -16,4 +26,22 @@ input.addEventListener("change", (event) => {
 //event input => run after change input
 input.addEventListener("input", (event) => {
     console.log("input", event.target.value)
+    renderProduct(products, event.target.value)
 });
+
+function renderProduct(p, f) {
+    const filterdProducts = p.filter(element => {
+        return element.title.toLowerCase().includes(f.toLowerCase())
+    });
+
+    root.remove()
+    root = document.createElement("root")
+
+    body.appendChild(root)
+
+    filterdProducts.forEach(items => {
+        const h3 = document.createElement("h4")
+        h3.textContent = items.title
+        root.appendChild(h3)
+    });
+}
