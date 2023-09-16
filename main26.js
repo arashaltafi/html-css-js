ecf684ac9f93f60336eb52cab2d05733a0817eca
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-16 15:46:53 +0430

    a

--- main26.js
+++ main26.js
@@ -1,18 +1,31 @@
 //DOM
+//click, button
 
-const courses = [
-    { title: "course 1", isExist: true },
-    { title: "course 2", isExist: false },
-    { title: "course 3", isExist: true },
-    { title: "course 4", isExist: false }
+const products = [
+    { title: "product 1", isExist: true },
+    { title: "product 2", isExist: false },
+    { title: "product 3", isExist: true },
+    { title: "product 4", isExist: false }
 ]
-const existCourse = courses.filter(element => element.isExist)
+const existProducts = products.filter(element => element.isExist)
 
+
+const btnAdd = document.querySelector("#add-product");
+const btnRemove = document.querySelector("#remove-product");
+const productList = document.querySelector("#product-list");
 const body = document.querySelector("body");
 
-existCourse.forEach((items) => {
-    const p = document.createElement("h2")
-    p.textContent = items.title
+btnAdd.addEventListener("click", (event) => {
+    console.log("clicked", event.target.textContent)
+    existProducts.forEach((items) => {
+        const p = document.createElement("h2")
+        p.textContent = items.title
+    
+        body.appendChild(p)
+    })
+});
 
-    body.appendChild(p)
-})
+btnRemove.addEventListener("click", (event) => {
+    console.log("clicked", event.target.textContent)
+    productList.remove();
+});
