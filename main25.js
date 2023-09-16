85338b4d73214cb79b129ac32be466b09f223ab5
Arash Altafi <arashaltafi1377@gmail.com>
2023-09-16 14:26:53 +0430

    a

--- main25.js
+++ main25.js
@@ -12,21 +12,32 @@ console.log(h2)
 //get first tag p
 const p1 = document.querySelector("p");
 console.log(p1)
+//update text
 p1.textContent = "add new text from js";
 
 //get tag with class name root
+//if you have class name in html => querySelector(".root")
+//if you have id name in html => querySelector("#root")
 const root = document.querySelector(".root");
 console.log(root)
 
 //get tag p with class name p-1 in tag root with class name root
 const p2 = document.querySelector(".root .p-2");
 console.log(p2);
+//remove
 p2.remove();
 
-//get all tags p
+//get all tags p (in array)
 const pAll = document.querySelectorAll("p");
 console.log(pAll)
 
 pAll.forEach((item) => {
     console.log(item.textContent)
 })
+
+//add element to dom
+//if you have class name in html => querySelector(".root")
+//if you have id name in html => querySelector("#root")
+const pNew = document.createElement("p");
+pNew.textContent = "Add New Eelement From JS";
+document.querySelector(".root").appendChild(pNew)
