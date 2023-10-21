e90648e66bd00613136143b35d9740176c90d1c6
Arash Altafi <arashaltafi1377@gmail.com>
2023-10-21 22:03:55 +0330

    A

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,35 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '2rem'
+      padding: {
+        DEFAULT: '2rem',
+        sm: '3rem'
+      }
     },
     extend: {
       width: {
-        f: '100rem'
+        400: '100rem'
       },
       height: {
-        f: '100rem'
+        400: '100rem'
+      },
+      colors: {
+        "primary": "#2979FF",
+        "secondary": {
+          DEFAULT: "#009688",
+          "50": "#E0F2F1",
+          "100": "#B2DFDB",
+          "200": "#80CBC4",
+          "300": "#4DB6AC",
+          "400": "#26A69A",
+          "500": "#009688",
+          "600": "#00897B",
+          "700": "#00796B",
+          "800": "#00695C",
+          "900": "#004D40",
+          "lighter": "#B2DFDB",
+          "darker": "#004D40",
+        }
       }
     },
   },
