1f56be04c3de32788d9c3f2eaf09fc519d91ba28
Arash Altafi <arashaltafi1377@gmail.com>
2023-10-25 22:54:33 +0330

    A

--- tailwindcss/sample5/scripts/app.js
+++ tailwindcss/sample5/scripts/app.js
@@ -1,17 +1,15 @@
 //toggle theme
-const btn = document.getElementById('changeTheme');
+const btn = document.getElementById('toggle-theme');
 const html = document.documentElement;
-const icon = document.getElementById('iconTheme');
 
 btn.addEventListener('click', () => {
-    if (html.classList.contains('dark')) {
-        html.classList.remove('dark');
-        localStorage.setItem("theme", "light")
+    if (localStorage.theme === "dark"){
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme" , "light");
     } else {
-        html.classList.add('dark');
-        localStorage.setItem("theme", "dark")
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme" , "dark");
     }
-    location.reload()
 })
 
 if (localStorage.getItem("theme") === null) {
@@ -22,11 +20,9 @@ if (localStorage.getItem("theme") === null) {
         if (newColorScheme == "dark") {
             console.log("if add dark")
             html.classList.add('dark');
-            icon.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#sun');
         } else {
             console.log("if add dark")
             html.classList.remove('dark');
-            icon.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#moon');
         }
     });
 
@@ -34,21 +30,17 @@ if (localStorage.getItem("theme") === null) {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         console.log("change theme add dark")
         html.classList.add('dark');
-        icon.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#sun');
     } else {
         console.log("change theme remove dark")
         html.classList.remove('dark');
-        icon.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#moon');
     }
 } else {
     console.log("else", localStorage.getItem("theme"))
     if (localStorage.getItem("theme") === "dark") {
         console.log("else add dark")
         html.classList.add('dark');
-        icon.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#sun');
     } else {
         console.log("else remove dark")
         html.classList.remove('dark');
-        icon.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#moon');
     }
 }
