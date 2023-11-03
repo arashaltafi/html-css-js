c13290d44b09628c6acafecbc7e967550b2a88fe
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-03 17:26:41 +0330

    A

--- js-fronthook/fetch/app.js
+++ js-fronthook/fetch/app.js
@@ -9,8 +9,8 @@ const getDataAxios = document.querySelector("#get-data-axios")
 postData.addEventListener('click', () => fetchData2("https://jsonplaceholder.typicode.com/users", "POST"))
 getData.addEventListener('click', () => fetchData2("https://jsonplaceholder.typicode.com/users/2", "GET"))
 
-postDataAxios.addEventListener('click', () => axiosData1("https://jsonplaceholder.typicode.com/users", "POST"))
-getDataAxios.addEventListener('click', () => axiosData1("https://jsonplaceholder.typicode.com/users/2", "GET"))
+postDataAxios.addEventListener('click', () => axiosData2("https://jsonplaceholder.typicode.com/users", "POST"))
+getDataAxios.addEventListener('click', () => axiosData2("https://jsonplaceholder.typicode.com/users/2", "GET"))
 
 function fetchData1(url, method) {
     const userData = {
@@ -105,4 +105,32 @@ function axiosData1(url, method) {
         .then(response => {
             console.log(response.data)
         })
+}
+
+async function axiosData2(url, method) {
+    const userData = {
+        id: 1,
+        name: "arash",
+        username: "arashaltafi",
+        phone: "+989187677641",
+        email: "arashaltafi1377@gmail.com",
+        website: "arashaltafi.ir"
+    }
+
+    const header = {
+        'Content-type': 'application/json; charset=UTF-8'
+    }
+
+    let axiosPromise;
+    if (method == 'POST') {
+        axiosPromise = axios.post(url, userData, header)
+    } else {
+        axiosPromise = axios.get(url)
+    }
+    try {
+        const response = await axiosPromise
+        console.log(response.data)
+    } catch (error) {
+        console.error('Error:', error)
+    }
 }
