2690894aa5ddbb68e8c2191a63e3f8c1cbd7ef1e
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-03 16:08:42 +0330

    A

--- js-fronthook/fetch/app.js
+++ js-fronthook/fetch/app.js
@@ -3,11 +3,30 @@
 const postData = document.querySelector("#post-data")
 const getData = document.querySelector("#get-data")
 
-postData.addEventListener('click', () => fetchData1(""))
-getData.addEventListener('click', () => fetchData1(""))
+postData.addEventListener('click', () => fetchData2("https://jsonplaceholder.typicode.com/users", "POST"))
+getData.addEventListener('click', () => fetchData2("https://jsonplaceholder.typicode.com/users/2", "GET"))
 
-function fetchData1(url) {
-    fetch(url)
+function fetchData1(url, method) {
+    const userData = {
+        id: 1,
+        name: "arash",
+        username: "arashaltafi",
+        phone: "+989187677641",
+        email: "arashaltafi1377@gmail.com",
+        website: "arashaltafi.ir"
+    }
+
+    let fetchPromise;
+    if (method == 'POST') {
+        fetchPromise = fetch(url, {
+            method: method,
+            body: JSON.stringify(userData),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' }
+        })
+    } else {
+        fetchPromise = fetch(url, { method: method })
+    }
+    fetchPromise
         .then(response => {
             return response.json()
         })
@@ -18,18 +37,32 @@ function fetchData1(url) {
             console.error('Error:', error);
         });
 }
-fetchData1('https://jsonplaceholder.typicode.com/todos/1')
 
-/////////////////////////////////
+async function fetchData2(url, method) {
+    const userData = {
+        id: 1,
+        name: "arash",
+        username: "arashaltafi",
+        phone: "+989187677641",
+        email: "arashaltafi1377@gmail.com",
+        website: "arashaltafi.ir"
+    }
 
-async function fetchData2(url) {
     try {
-        const response = await fetch(url)
+        let fetchPromise;
+        if (method == 'POST') {
+            fetchPromise = fetch(url, {
+                method: method,
+                body: JSON.stringify(userData),
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
+            })
+        } else {
+            fetchPromise = fetch(url, { method: method })
+        }
+        const response = await fetchPromise
         const json = await response.json()
         console.log(json)
     } catch (error) {
         console.error('Error:', error)
     }
 }
-
-fetchData2('https://jsonplaceholder.typicode.com/todos/1');
