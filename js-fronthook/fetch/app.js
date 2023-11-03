ee60f771eb4f742d186772db6ec8fef6882f9b14
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-03 15:19:29 +0330

    A

--- js-fronthook/fetch/app.js
+++ js-fronthook/fetch/app.js
@@ -1,14 +1,22 @@
+//post , get , put , delete
+
+const postData = document.querySelector("#post-data")
+const getData = document.querySelector("#get-data")
+
+postData.addEventListener('click', () => fetchData1(""))
+getData.addEventListener('click', () => fetchData1(""))
+
 function fetchData1(url) {
     fetch(url)
-    .then(response => {
-        return response.json()
-    })
-    .then(json => {
-        console.log(json)
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then(response => {
+            return response.json()
+        })
+        .then(json => {
+            console.log(json)
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 fetchData1('https://jsonplaceholder.typicode.com/todos/1')
 
@@ -17,8 +25,9 @@ fetchData1('https://jsonplaceholder.typicode.com/todos/1')
 async function fetchData2(url) {
     try {
         const response = await fetch(url)
-        console.log(response.json())
-    } catch(error) {
+        const json = await response.json()
+        console.log(json)
+    } catch (error) {
         console.error('Error:', error)
     }
 }
