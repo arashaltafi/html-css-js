07597b4b91930e0c8672413aba77cf6f024b71e6
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-04 18:28:57 +0330

    A

--- js-mega/sample1/app.js
+++ js-mega/sample1/app.js
@@ -31,3 +31,27 @@ box.addEventListener('mouseenter',  () => {
 box.addEventListener('mouseleave',  () => {
     console.log('mouseleave')
 })
+
+///////////////
+
+const test3 = document.getElementById('test3');
+test3.addEventListener('click',  (event) => {
+    console.log(event.type)
+    console.log(event.target)
+    console.log(event.target.nodeName)
+    console.log(`event.clientX ${event.clientX}`, `event.clientY ${event.clientY}`)
+    console.log(`event.offsetX ${event.offsetX}`, `event.offsetY ${event.offsetY}`)
+})
+
+const input1 = document.getElementById('input1');
+input1.addEventListener('keydown', () => {
+    console.log("keydown")
+})
+
+input1.addEventListener('keypress', () => {
+    console.log("keypress")
+})
+
+input1.addEventListener('keyup', () => {
+    console.log("keyup")
+})
