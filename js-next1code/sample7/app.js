f8d8401a985e303be4efc0dce9276947a36e25f9
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-05 23:01:19 +0330

    A

--- js-next1code/sample7/app.js
+++ js-next1code/sample7/app.js
@@ -1,10 +1,17 @@
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
+search.required = true
+search.disabled = false
+search.readOnly = false
+
 search.addEventListener('keydown', (e) => {
     if (e.key == "Enter") {
         form.setAttribute('action', 'https://google.com/search')
         form.setAttribute('method', 'GET')
         form.setAttribute('target', '_blank')
+        setTimeout(() => {
+            search.value = ""
+        }, 100);
     }
 })
