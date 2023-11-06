f49d21e1d1bc0eacef493d2280b1bb989e8bd71c
آرش الطافي <altafi.a@Nahad.ir>
2023-11-06 09:22:12 +0330

    A

--- js-next1code/sample8/app.js
+++ js-next1code/sample8/app.js
@@ -13,15 +13,31 @@ lName.addEventListener('focus', (e) => {
     lName.select();
 })
 
-day.addEventListener('blur', () => {
+// day.addEventListener('blur', () => {
+//     if (day.value >= 1 && day.value <= 31) {
+//         day.nextElementSibling.innerHTML = ""
+//     } else {
+//         day.nextElementSibling.innerHTML = "لطفا روز را به صورت صحیح 1 تا 31 وارد نمایید"
+//     }
+// })
+
+// month.addEventListener('blur', () => {
+//     if (month.value >= 1 && month.value <= 12) {
+//         month.nextElementSibling.innerHTML = ""
+//     } else {
+//         month.nextElementSibling.innerHTML = "لطفا ماه را به صورت صحیح 1 تا 12 وارد نمایید"
+//     }
+// })
+
+day.addEventListener('change', () => {
     if (day.value >= 1 && day.value <= 31) {
         day.nextElementSibling.innerHTML = ""
     } else {
-        day.nextElementSibling.innerHTML = "لطفا روز را به صورت صحیح 1 تا 31 وارد نمایید"
+        day.previousElementSibling.innerHTML = "لطفا روز را به صورت صحیح 1 تا 31 وارد نمایید"
     }
 })
 
-month.addEventListener('blur', () => {
+month.addEventListener('change', () => {
     if (month.value >= 1 && month.value <= 12) {
         month.nextElementSibling.innerHTML = ""
     } else {
