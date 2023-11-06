61a9898ee48629af4d6252bb5175237309b391e0
آرش الطافي <altafi.a@Nahad.ir>
2023-11-06 08:46:21 +0330

    A

--- js-next1code/sample8/app.js
+++ js-next1code/sample8/app.js
@@ -1,5 +1,7 @@
 const fName = document.getElementById('firstname');
 const lName = document.getElementById('lastname');
+const day = document.getElementById('day');
+const month = document.getElementById('month');
 
 fName.focus()
 
@@ -9,4 +11,20 @@ fName.addEventListener('focus', (e) => {
 
 lName.addEventListener('focus', (e) => {
     lName.select();
+})
+
+day.addEventListener('blur', () => {
+    if (day.value >= 1 && day.value <= 31) {
+        day.nextElementSibling.innerHTML = ""
+    } else {
+        day.nextElementSibling.innerHTML = "لطفا روز را به صورت صحیح وارد نمایید"
+    }
+})
+
+month.addEventListener('blur', () => {
+    if (month.value >= 1 && month.value <= 12) {
+        month.nextElementSibling.innerHTML = ""
+    } else {
+        month.nextElementSibling.innerHTML = "لطفا ماه را به صورت صحیح وارد نمایید"
+    }
 })
