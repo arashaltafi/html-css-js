8b85c55467119587d5a3f3a1ea1415f4d6911e2e
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-06 22:53:03 +0330

    A

--- js-next1code/sample10/app.js
+++ js-next1code/sample10/app.js
@@ -6,65 +6,67 @@ const locationReload = document.getElementById('btn-location-reload');
 const locationReloadWithoutCache = document.getElementById('btn-location-reload-without-cache');
 const locationAssign = document.getElementById('btn-location-assign');
 const locationReplace = document.getElementById('btn-location-replace');
-const windowOpenTab = document.getElementById('btn-Window-open-tab');
-const windowOpenCurrent = document.getElementById('btn-Window-open-current');
-const windowOpenWindow = document.getElementById('btn-Window-open-window');
-const windowCloseCurrent = document.getElementById('btn-Window-close-current');
-const windowCloseLast = document.getElementById('btn-Window-close-last');
+const windowOpenTab = document.getElementById('btn-window-open-tab');
+const windowOpenCurrent = document.getElementById('btn-window-open-current');
+const windowOpenWindow = document.getElementById('btn-window-open-window');
+const windowCloseCurrent = document.getElementById('btn-window-close-current');
+const windowCloseLast = document.getElementById('btn-window-close-last');
 const btnAlert = document.getElementById('btn-alert');
 const btnPrompt = document.getElementById('btn-prompt');
 const btnConfirm = document.getElementById('btn-confirm');
 
 historyBack.addEventListener('click', () => {
-
+    window.history.back()
 })
 
 historyForward.addEventListener('click', () => {
-    
+    window.history.forward()
 })
 
 historyGo.addEventListener('click', () => {
-    
+    window.history.go(-2)
 })
 
 historyLength.addEventListener('click', () => {
-    
+    const length = window.history.length
+    alert(`length is: ${length}`)
 })
 
 locationReload.addEventListener('click', () => {
-    
+    window.location.reload() //default input is false
 })
 
 locationReloadWithoutCache.addEventListener('click', () => {
-    
+    window.location.reload(true)
 })
 
 locationAssign.addEventListener('click', () => {
-    
+    window.location.assign('https://arashaltafi.ir')
 })
 
 locationReplace.addEventListener('click', () => {
-    
+    window.location.replace('https://arashaltafi.ir')
 })
 
 windowOpenTab.addEventListener('click', () => {
-    
+    window.open('https://arashaltafi.ir')
 })
 
 windowOpenCurrent.addEventListener('click', () => {
-    
+    window.open('https://arashaltafi.ir', '_self')
 })
 
+let wLast;
 windowOpenWindow.addEventListener('click', () => {
-    
+    wLast = window.open('https://arashaltafi.ir', '_blank', 'width=400 , height=400')
 })
 
 windowCloseCurrent.addEventListener('click', () => {
-    
+    window.close()
 })
 
 windowCloseLast.addEventListener('click', () => {
-    
+    wLast.close()
 })
 
 btnAlert.addEventListener('click', () => {
