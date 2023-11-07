3621de7b037c5c7da0aa1b6b9fd4aca81c233ef1
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-07 21:39:49 +0330

    A

--- js-next1code/sample14/app.js
+++ js-next1code/sample14/app.js
@@ -1,13 +1,21 @@
-const sec = document.querySelector(".sec");
-const min = document.querySelector(".min");
-const hour = document.querySelector(".hour");
-let deg = 6
+const second = document.querySelector("#second");
+const minute = document.querySelector("#minute");
+const hour = document.querySelector("#hour");
 
 setInterval(() => {
     clockHandler()
 }, 1000);
 
 function clockHandler() {
-    sec.style.transform = `rotateZ(${deg}deg)`
-    deg += 6;
+    d = new Date();
+    hr = d.getHours();
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    hr_rotation = 30 * hr + min / 2; //converting current time
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
 }
