c199adf39e0470ecbaca6609550468455dd5efe1
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-09 22:51:11 +0330

    A

--- js-next1code/sample17/app.js
+++ js-next1code/sample17/app.js
@@ -3,11 +3,16 @@ const btnSendXml = document.getElementById('btn-send-xml')
 
 btnSendXml.addEventListener('click', () => {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users/2', true);
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
     xhr.onload = function() {
-        console.log(this.responseText);
-        let obj = JSON.parse(this.responseText)
-        result.textContent = obj.name
+        if (this.status >= 200 && this.status < 400) {
+            console.log(this.responseText);
+            let obj = JSON.parse(this.responseText)
+            result.textContent = obj.title
+        } else {
+            console.error('Error:', this.status, this.statusText);
+        }
+        
     }
     xhr.onerror = function() {
         result.textContent = this.error
