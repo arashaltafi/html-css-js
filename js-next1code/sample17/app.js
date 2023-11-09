6737d2d4e1ab49a5e85f47abe54c56828b979b10
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-09 23:07:23 +0330

    A

--- js-next1code/sample17/app.js
+++ js-next1code/sample17/app.js
@@ -1,21 +1,44 @@
 const result = document.getElementById('result')
 const btnSendXml = document.getElementById('btn-send-xml')
 
-btnSendXml.addEventListener('click', () => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
-    xhr.onload = function() {
-        if (this.status >= 200 && this.status < 400) {
-            console.log(this.responseText);
-            let obj = JSON.parse(this.responseText)
-            result.textContent = obj.title
-        } else {
-            console.error('Error:', this.status, this.statusText);
-        }
-        
+btnSendXml.addEventListener('click', async () => {
+    try {
+        const response = await callApiXmlHTTPRequest();
+        console.log('Response Status:', response.status);
+        console.log('Response Headers:', response.headers);
+        console.log('Response Data:', response.data);
+        result.textContent = response.data.title
+    } catch (error) {
+        console.error(error.message);
+        result.textContent = error.message
     }
-    xhr.onerror = function() {
-        result.textContent = this.error
-    }
-    xhr.send();
 })
+
+function callApiXmlHTTPRequest() {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
+        xhr.withCredentials = true;
+        xhr.setRequestHeader('test', 'arash')
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 400) {
+                const status = xhr.status
+                const headers = xhr.getAllResponseHeaders();
+                const data = JSON.parse(xhr.responseText);
+                resolve({ status, headers, data });
+            } else {
+                reject(new Error(`Request failed with status ${xhr.status}`));
+            }
+        }
+        xhr.onprogress = function (evt) {
+            if (evt.lengthComputable) {
+                console.log("evt.loaded", evt.loaded);
+                console.log("evt.total", evt.total);
+            }
+        }
+        xhr.onerror = function () {
+            result.textContent = this.error
+        }
+        xhr.send();
+    })
+}
