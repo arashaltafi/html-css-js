1a87b77e0d9ce26ef620494917f471dc37cf079b
آرش الطافي <altafi.a@Nahad.ir>
2023-11-15 08:13:57 +0330

    A

--- js-next1code/sample14/app.js
+++ js-next1code/sample14/app.js
@@ -1,21 +1,39 @@
 const second = document.querySelector("#second");
 const minute = document.querySelector("#minute");
 const hour = document.querySelector("#hour");
+const timer = document.querySelector("#timer");
 
 setInterval(() => {
-    clockHandler()
+  clockHandler()
 }, 1000);
 
 function clockHandler() {
-    d = new Date();
-    hr = d.getHours();
-    min = d.getMinutes();
-    sec = d.getSeconds();
-    hr_rotation = 30 * hr + min / 2; //converting current time
-    min_rotation = 6 * min;
-    sec_rotation = 6 * sec;
+  d = new Date();
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
+  hr_rotation = 30 * hr + min / 2; //converting current time
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
 
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
-    second.style.transform = `rotate(${sec_rotation}deg)`;
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+}
+
+
+setInterval(() => {
+  var d = new Date();
+  var h = addLeadingZero(d.getHours());
+  var m = addLeadingZero(d.getMinutes());
+  var s = addLeadingZero(d.getSeconds());
+
+  const finalTime = `${h} : ${m} : ${s}`
+  timer.textContent = finalTime
+  console.log(finalTime)
+  
+}, 1000);
+
+function addLeadingZero(number) {
+  return (number < 10 ? "0" : "") + number;
 }
