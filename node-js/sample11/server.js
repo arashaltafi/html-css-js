1b06d27cb854a3bd77a47cdf3cff66a33737cfe1
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-21 22:31:17 +0330

    a

--- node-js/sample11/server.js
+++ node-js/sample11/server.js
@@ -4,12 +4,43 @@ const port = 8080;
 
 // GET method route
 app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
+    // res.sendStatus(200)
+    // res.send('GET request to the homepage')
+    res.send({
+        status: true,
+        message: 'Hello From Express'
+    })
 })
+
+// GET method route From Test
+app.get('/test', (req, res) => {
+    // res.sendStatus(200)
+    // res.send('GET request to the homepage')
+    res.send({
+        status: true,
+        message: 'Hello From Express Test'
+    })
+})
+
 
 // POST method route
 app.post('/', (req, res) => {
     res.send('POST request to the homepage')
+})
+
+// PATCH method route
+app.patch('/', (req, res) => {
+    res.send('PATCH request to the homepage')
+})
+
+// DELETE method route
+app.delete('/', (req, res) => {
+    res.send('DELETE request to the homepage')
+})
+
+// PUT method route
+app.put('/', (req, res) => {
+    res.send('PUT request to the homepage')
 })
 
 app.all('/', (req, res, next) => {
