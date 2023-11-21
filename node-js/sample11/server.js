81663b13869fb826c99b7f15be4b635157fcb8e0
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-21 22:09:37 +0330

    a

--- node-js/sample11/server.js
+++ node-js/sample11/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// GET method route
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+})
+
+app.all('/', (req, res, next) => {
+    console.log('Accessing the secret section ...')
+    next() // pass control to the next handler
+})
+
 app.listen(port, () => {
     console.log(`server is running at port ${port} ...`)
 })
