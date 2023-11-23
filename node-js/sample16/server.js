38b219dd6953eddfc5ccce87172ad3e9d1f7b3f9
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-23 23:29:05 +0330

    a

--- node-js/sample16/server.js
+++ node-js/sample16/server.js
@@ -7,6 +7,21 @@ const getAll = async () => {
     return { rows, fields };
 }
 
+const insertUser = async () => {
+    const newUserData = {
+        name: 'test name insert',
+        family: 'test family insert',
+        mobile: '0911111111',
+        status: 1
+    };
+    const [rows, fields] = await db.query(`INSERT INTO users SET ?` , newUserData)
+    console.log("rows", rows);
+    console.log("fields", fields);
+    console.log("-----------------------");
+}
+
+insertUser();
+
 getAll().then(({ rows, fields }) => {
     console.log("rows", rows);
     console.log("fields", fields);
