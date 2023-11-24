acd79d5ba89468febc83258ba9716cd4b14c3a25
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-24 12:21:48 +0330

    a

--- node-js/sample17/app/bootstrap/index.js
+++ node-js/sample17/app/bootstrap/index.js
@@ -1,5 +1,8 @@
 const hbs = require('express-handlebars');
+const path = require('path');
 
 module.exports = app => {
-    
+    app.engine('handlebars', hbs());
+    app.set('view engine', 'handlebars');
+    app.set('views', path.join(__dirname, '../views'))
 };
--- node-js/sample17/app/index.js
+++ node-js/sample17/app/index.js
@@ -4,6 +4,13 @@ const app = express();
 boot(app);
 const port = process.env.APP_PORT;
 
+app.get('/', (req, res) => {
+    res.render('main', {
+        layout: false,
+        userID: 456
+    });
+})
+
 module.exports = () => {
     app.listen(port, () => {
         console.log(`server is running at port ${port} ...`)
