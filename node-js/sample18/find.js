dd7136b450ea097070423a3a1589b4aa1fda3724
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-25 11:13:44 +0330

    a

--- node-js/sample18/find.js
+++ node-js/sample18/find.js
@@ -4,8 +4,11 @@ const findSample = async () => {
     try {
         const db = await connectToMongo();
         const collection = db.collection('sample');
-        const result = await collection.find({price: {$gt: 2000}}, {name: 1}).toArray();
-        console.log('Query result:', result.map(res => res.name));
+        const result = await collection.find({ price: { $gt: 2000 } }, { name: 1 }).toArray();
+        console.log('Query result:', result.map((res) => ({
+            name: res.name,
+            price: res.price
+        })));
     } catch (error) {
         // Handle errors
         console.error('Error in someFunction:', error.message);
