5f6adb89128e94cbb2fa3dfe3b30e5921cf98cac
Arash Altafi <arashaltafi1377@gmail.com>
2023-11-26 23:19:28 +0330

    a

--- node-js/sample19/app/routers/index.js
+++ node-js/sample19/app/routers/index.js
@@ -1,6 +1,8 @@
 const userRouter = require('./users');
+const sessionRouter = require('./session');
 const auth = require('../middlewares/auth')
 
 module.exports = (app) => {
     app.use('/api/v1/users', [auth], userRouter);
+    app.use('/api/v1/session', sessionRouter);
 }
