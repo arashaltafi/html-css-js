e120fd7357ab89a2f52f4697c6be0d9a8ce09b12
unknown <NahadAdmin@Cli3-034.Nahad.ir>
2023-12-04 09:37:27 +0330

    a

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
+const cacheVersion = 16;
+const activeCache = {
+    static: `static-v${cacheVersion}`,
+    dynamic: `dynamic-v${cacheVersion}`
+}
+
 self.addEventListener('install', (event) => {
     console.log('service worker install successful');
     self.skipWaiting();
 
     event.waitUntil(
-        caches.open('cache-v1').then((cache) => {
-            console.log('cache open successful');
+        caches.open(activeCache.static).then((cache) => {
+            console.log('cache static open successful');
             cache.addAll([
                 '/',
                 '/app.js'
@@ -15,6 +21,20 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     console.log('service worker activate successful');
+    const activeCacheNames = Object.values(activeCache);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map(element => {
+                    console.log(element);
+                    if (!activeCacheNames.includes(element)) {
+                        return caches.delete(element)
+                    }
+                })
+            )
+        })
+    )
 })
 
 self.addEventListener('fetch', (event) => {
@@ -24,7 +44,13 @@ self.addEventListener('fetch', (event) => {
             if (res) {
                 return res
             } else {
-                return fetch(event.request)
+                return fetch(event.request).then(res => {
+                    caches.open(activeCache.dynamic).then((cache) => {
+                        console.log('cache dynamic open successful');
+                        cache.put(event.request, res.clone());
+                        return res;
+                    })
+                })
             }
         }).catch((err) => {
             return fetch(event.request)
