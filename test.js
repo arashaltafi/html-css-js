b70444855e71832fdf7b1278737e9a3392dbe343
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-14 23:27:41 +0330

    a

--- test.js
+++ test.js
@@ -1,2 +1,50 @@
 var num1 = 12;
 var num2 = 11;
+var firstName = 'arash';
+var lastName = 'altafi';
+var arrayNumber = [1, 2, 3, 4, 5];
+var arrayString = ['a', 'b', 'c'];
+var arrayMixed = [1, 'a', true];
+var person = {
+    fName: 'arash',
+    lName: 'altafi',
+    age: 25,
+    isMale: true
+};
+var mixed = 'arash';
+mixed = 26;
+var object;
+object = {
+    fName: 'arash',
+    lName: 'altafi',
+    age: 25,
+    isMale: true
+};
+var anySample;
+anySample = 26;
+anySample = 'arash';
+anySample = true;
+var user;
+user = {
+    fName: 'arash',
+    lName: 'altafi'
+};
+var sampleFunction;
+sampleFunction = function () {
+    console.log('test function');
+};
+var sampleFunction2 = function (name, age) {
+    console.log('hello ', name, age);
+};
+sampleFunction2('arash', 25);
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+add(12, 11);
+var add2 = function (num1, num2, num3) { return num1 + num2 + (num3 || 0); };
+add2(12, 11);
+var add3 = function (num1, num2, num3) {
+    if (num3 === void 0) { num3 = 0; }
+    return num1 + num2 + num3;
+};
+add3(12, 11);
--- test.ts
+++ test.ts
@@ -1,2 +1,67 @@
 let num1: Number = 12
 let num2: Number = 11
+
+const firstName: string = 'arash'
+const lastName: string = 'altafi'
+
+const arrayNumber = [1, 2, 3, 4, 5]
+const arrayString = ['a', 'b', 'c']
+const arrayMixed = [1, 'a', true]
+
+const person = {
+    fName: 'arash',
+    lName: 'altafi',
+    age: 25,
+    isMale: true
+}
+
+let mixed: (string | number) = 'arash'
+mixed = 26
+
+let object: {
+    fName: string,
+    lName: string,
+    age: number,
+    isMale: boolean
+}
+object = {
+    fName: 'arash',
+    lName: 'altafi',
+    age: 25,
+    isMale: true
+}
+
+let anySample: any;
+anySample = 26;
+anySample = 'arash'
+anySample = true
+
+let user: {
+    fName: string,
+    lName: string
+}
+user = {
+    fName: 'arash',
+    lName: 'altafi'
+}
+
+let sampleFunction: Function;
+sampleFunction = () => {
+    console.log('test function');
+}
+
+let sampleFunction2 = (name: string, age: number) => {
+    console.log('hello ', name, age);
+}
+sampleFunction2('arash', 25)
+
+const add = (num1: number, num2: number): number => {
+    return num1 + num2
+}
+add(12, 11);
+
+const add2 = (num1: number, num2: number, num3?: number): number => num1 + num2 + (num3 || 0)
+add2(12, 11);
+
+const add3 = (num1: number, num2: number, num3: number = 0): number => num1 + num2 + num3
+add3(12, 11);
